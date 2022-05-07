2468a4f41334b3aed65f57c24fd7abb86d57b94b
Mohammed <arrahmaan9901@gmail.com>
2022-05-07 18:09:19 +0530

    created CertificatePage Component

---
+++ src/Components/Certifications/CertificatePage.js
@@ -0,0 +1,64 @@
+import Container from '@mui/material/Container'
+import Typography from '@mui/material/Typography';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import Box from '@mui/material/Box'
+import { data } from '../../data'
+
+import { Image } from '@mui/icons-material';
+import { useParams } from "react-router-dom";
+
+
+export default function CertificatePage() {
+    let params = useParams();
+
+    return (
+        <>
+            <Box
+                px={{ xs: 3, sm: 10 }}
+                py={{ xs: 5, sm: 10 }}
+                bgcolor={'white'}
+                color={'white'} >
+                <Container sx={{ width: '100%' }}>
+                    <Typography textAlign="center" fontFamily={'Righteous'} fontSize={'40px'} color={'black'} pt={{ xs: 1, sm: 2 }}>Certificate</Typography>
+                    <Box display={'flex'} flexDirection={'column'} bgcolor={'#eee'} borderRadius={'20px'} padding={'10px'}>
+
+
+
+                        <Typography color={'black'} gutterBottom variant="h5" component="div" textAlign={'center'} paddingBottom={'8px'}>
+                            {data.certifications[params.i].title}
+                        </Typography>
+                        <img src={data.certifications[params.i].thumbnail} />
+                        <Box display={'flex'} flexDirection={'row'} >
+                            <Typography color={'black'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                                {"Date: "}
+                            </Typography>
+                            <Typography color={'black'} gutterBottom variant="h6" component="div"  >
+                                {`${data.certifications[params.i].date}`}
+                            </Typography>
+                        </Box>
+
+                        <Box display={'flex'} flexDirection={'row'} >
+                            <Typography color={'black'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                                {`Institution:`}
+                            </Typography>
+                            <Typography color={'black'} gutterBottom variant="h6" component="div"  >
+                                {`${data.certifications[params.i].Institution}`}
+                            </Typography>
+                        </Box>
+                        <Box display={'flex'} flexDirection={'row'} >
+                            <Typography color={'black'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                                {`Description: `}
+                            </Typography>
+                            <Typography color={'black'} gutterBottom variant="h6" component="div"  >
+                                {`${data.certifications[params.i].description}`}
+                            </Typography>
+                        </Box>
+                    </Box>
+                </Container>
+            </Box>
+        </>
+    );
+}
