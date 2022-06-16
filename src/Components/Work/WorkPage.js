62118309a85d1f129d834ae9694455273a6bb96c
Mohammed <arrahmaan9901@gmail.com>
2022-06-16 14:40:50 +0530

    dynamic workpage

--- src/Components/Work/WorkPage.js
+++ src/Components/Work/WorkPage.js
@@ -1,13 +1,63 @@
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'
-import { data } from '../../data'
-
+import {
+    useNavigate
+} from "react-router-dom";
+import { getDatabase, ref, child, get } from "firebase/database";
+import * as React from 'react';
 import { useParams } from "react-router-dom";
+import { useEffect } from 'react';
 
-
+var workItems = [];
 export default function WorkPage() {
-    let params = useParams();
+    const params = useParams();
+    var user = params.username;
+
+    let navigate = useNavigate();
+    const nav = (index) => {
+        navigate(`/${user}/home/workpage/${index}`)
+    }
+
+    const dbRef = ref(getDatabase());
+    const [companyPlaceHolder, setcompanyPlaceholder] = React.useState([]);
+    const [locationPlaceHolder, setlocationPlaceholder] = React.useState([]);
+    const [titlePlaceHolder, settitlePlaceholder] = React.useState([]);
+    const [thumbnailPlaceHolder, setthumbnailPlaceholder] = React.useState([]);
+    const [datesPlaceHolder, setdatesPlaceholder] = React.useState([]);
+    const [descriptionPlaceHolder, setdescriptionPlaceholder] = React.useState([]);
+
+    useEffect(() => {
+        get(child(dbRef, `${user}/workinfo/work/`)).then((snapshot) => {
+            console.log("UserID", user);
+            if (snapshot.exists()) {
+                snapshot.forEach(function (item) {
+                    var itemVal = item.val();
+                    workItems.push(itemVal);
+                });
+                console.log("workItems", workItems.length);
+                workItems.map(
+                    (object, index) => (
+                        setValues(index)
+                    )
+                );
+            } else {
+                console.log("No data available in Education.js");
+            }
+        }).catch((error) => {
+            console.error("error ...", error);
+        });
+    }, []);
+
+    function setValues(i) {
+        setcompanyPlaceholder(oldArray => [...oldArray, workItems[i]["company"]]);
+        setlocationPlaceholder(oldArray => [...oldArray, workItems[i]["location"]]);
+        settitlePlaceholder(oldArray => [...oldArray, workItems[i]["title"]]);
+        setthumbnailPlaceholder(oldArray => [...oldArray, workItems[i]["thumbnail"]]);
+        setdatesPlaceholder(oldArray => [...oldArray, workItems[i]["dates"]]);
+        setdescriptionPlaceholder(oldArray => [...oldArray, workItems[i]["description"]]);
+    }
+
 
     return (
         <>
@@ -19,13 +69,13 @@ export default function WorkPage() {
                 <Container sx={{ width: '100%' }}>
                     <Typography textAlign="center" fontFamily={'Righteous'} fontSize={'40px'} color={'black'} pt={{ xs: 1, sm: 2 }}>Work Experience</Typography>
                     <Box display={'flex'} flexDirection={'column'} bgcolor={'#eee'} borderRadius={'20px'} padding={'10px'}>
-                        <img src={data.work[params.i].thumbnail} />
+                        <img src={thumbnailPlaceHolder[params.i]} />
                         <Box display={'flex'} flexDirection={'row'} >
                             <Typography color={'black'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
                                 {"Job: "}
                             </Typography>
                             <Typography color={'black'} gutterBottom variant="h6" component="div"  >
-                                {`${data.work[params.i].title}`}
+                                {`${titlePlaceHolder[params.i]}`}
                             </Typography>
                         </Box>
                         <Box display={'flex'} flexDirection={'row'} >
@@ -33,7 +83,7 @@ export default function WorkPage() {
                                 {"Company: "}
                             </Typography>
                             <Typography color={'black'} gutterBottom variant="h6" component="div"  >
-                                {`${data.work[params.i].company}`}
+                                {`${companyPlaceHolder[params.i]}`}
                             </Typography>
                         </Box>
 
@@ -42,7 +92,7 @@ export default function WorkPage() {
                                 {`Location:`}
                             </Typography>
                             <Typography color={'black'} gutterBottom variant="h6" component="div"  >
-                                {`${data.work[params.i].location}`}
+                                {`${locationPlaceHolder[params.i]}`}
                             </Typography>
                         </Box>
                         <Box display={'flex'} flexDirection={'row'} >
@@ -50,7 +100,7 @@ export default function WorkPage() {
                                 {`Date:`}
                             </Typography>
                             <Typography color={'black'} gutterBottom variant="h6" component="div"  >
-                                {`${data.work[params.i].dates}`}
+                                {`${datesPlaceHolder[params.i]}`}
                             </Typography>
                         </Box>
                         <Box display={'flex'} flexDirection={'row'} >
@@ -58,7 +108,7 @@ export default function WorkPage() {
                                 {`Description: `}
                             </Typography>
                             <Typography color={'black'} gutterBottom variant="h6" component="div"  >
-                                {`${data.work[params.i].description}`}
+                                {`${descriptionPlaceHolder[params.i]}`}
                             </Typography>
                         </Box>
                     </Box>
