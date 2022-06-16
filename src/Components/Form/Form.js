bd003afc5b8dc51f8a4b9f9e955d4ac1f8928d80
Mohammed <arrahmaan9901@gmail.com>
2022-06-16 22:22:24 +0530

    accept data from firebase and change

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -12,85 +12,188 @@ import {
     materialRenderers,
     materialCells,
 } from '@jsonforms/material-renderers';
-import { Button, Grid, Paper } from '@mui/material';
-import { getDatabase, ref, set } from "firebase/database";
+import { Button, Grid, Paper, Typography } from '@mui/material';
+import { getDatabase, child, ref, set, get } from "firebase/database";
 import DownloadFooter from '../footer/Download/DownloadFooter';
 import FormNav from '../Navbar/FormNav';
+import { setReadonly } from '@jsonforms/core';
 
+var originalinformationinitialdata = undefined;
+var originalsocialprofilesinitialdata = undefined;
+var originaleducationinitialdata = undefined;
+var originalworkinitialdata = undefined;
+var originalskillsinitialdata = undefined;
+var originalcertificatesinitialdata = undefined;
+var originalprojectsinitialdata = undefined;
+var originalgalleryinitialdata = undefined;
+
+var user;
+
+var infoFlag = false;
+var socialFlag = false;
+var educationFlag = false;
+var workFlag = false;
+var skillsFlag = false;
+var projectsFlag = false;
+var certificatesFlag = false;
+var galleryFlag = false;
+
+var infoData;
+var socialData;
+var educationData;
+var workData;
+var skillsData;
+var projectsData;
+var certificatesData;
+var galleryData;
 class Form extends React.Component {
 
     constructor(props) {
         super(props);
         console.log("this.props.userID", this.props.userID);
+        user = this.props.userID;
+        this.initializeData();
         this.state = {
-            informationdata: informationinitialdata,
-            socialprofilesdata: socialprofilesinitialdata,
-            educationdata: educationinitialdata,
-            workdata: workinitialdata,
-            skillsdata: skillsinitialdata,
-            certificatesdata: certificatesinitialdata,
-            projectsdata: projectsinitialdata,
-            gallerydata: galleryinitialdata
+            // informationdata: (originalinformationinitialdata != undefined) ? originalinformationinitialdata : informationinitialdata,
+            // socialprofilesdata: (originalsocialprofilesinitialdata != undefined) ? originalsocialprofilesinitialdata : socialprofilesinitialdata,
+            // educationdata: (originaleducationinitialdata != undefined) ? originaleducationinitialdata : educationinitialdata,
+            // workdata: (originalworkinitialdata != undefined) ? originalworkinitialdata : workinitialdata,
+            // skillsdata: (originalskillsinitialdata != undefined) ? originalskillsinitialdata : skillsinitialdata,
+            // certificatesdata: (originalcertificatesinitialdata != undefined) ? originalcertificatesinitialdata : certificatesinitialdata,
+            // projectsdata: (originalprojectsinitialdata != undefined) ? originalprojectsinitialdata : projectsinitialdata,
+            // gallerydata: (originalgalleryinitialdata != undefined) ? originalgalleryinitialdata : galleryinitialdata,
+            // read: false
         };
-
     }
+
+
+    initializeData() {
+        // console.log('Initialize data');
+
+        const dbRef = ref(getDatabase());
+        get(child(dbRef, `${user}/information/`)).then((snapshot) => {
+            // console.log("infoBegin", snapshot);
+            if (snapshot.exists()) {
+                console.log("Initialize informationdata", this.state.informationdata);
+                this.state.informationdata = snapshot.val();
+                infoData = snapshot.val();
+                infoFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/socialmediaprofilesinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.socialprofilesinitialdata = snapshot.val();
+                socialData = snapshot.val();
+                socialFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/educationinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.educationinitialdata = snapshot.val();
+                educationData = snapshot.val();
+                educationFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/workinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.workinitialdata = snapshot.val();
+                workData = snapshot.val();
+                workFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/skillsinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.skillsinitialdata = snapshot.val();
+                skillsData = snapshot.val();
+                skillsFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/certificatesinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.certificatesinitialdata = snapshot.val();
+                certificatesData = snapshot.val();
+                certificatesFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/projectsinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.projectsinitialdata = snapshot.val();
+                projectsData = snapshot.val();
+                projectsFlag = true;
+            }
+        });
+        get(child(dbRef, `${user}/galleryinfo/`)).then((snapshot) => {
+            if (snapshot.exists()) {
+                this.state.galleryinitialdata = snapshot.val();
+                galleryData = snapshot.val();
+                galleryFlag = true;
+            }
+        });
+
+        // if (infoFlag == true && socialFlag == true && educationFlag == true && workFlag == true && skillsFlag == true && projectsFlag == true && certificatesFlag == true && galleryFlag == true) this.state.read = true;
+    }
+
     update() {
         let authToken = sessionStorage.getItem('Auth Token')
-        if (authToken) {
-            console.log(authToken);
-        } else {
+        if (this.props.userID != "") {
+            if (authToken) {
+                console.log(authToken);
+            }
+            const db = getDatabase();
+            console.log("this.props.userID", this.props.userID);
+            set(ref(db, `${this.props.userID}/information`), {
+                name: this.state.informationdata["name"],
+                domain: this.state.informationdata["domain"],
+                image: this.state.informationdata["image"],
+                email: this.state.informationdata["email"],
+                whoami: this.state.informationdata["whoami"],
+                description: this.state.informationdata["description"],
+                location: this.state.informationdata["location"],
+            });
+            // console.log(this.state.socialprofilesdata.profiles.length);
+            // console.log(this.state.socialprofilesdata.profiles[0]["media"]);
+            set(ref(db, `${this.props.userID}/socialmediaprofilesinfo`), {
+                profiles: this.state.socialprofilesdata["profiles"],
+            });
+            set(ref(db, `${this.props.userID}/educationinfo`), {
+                education: this.state.educationdata["education"],
+            });
+
+
+            set(ref(db, `${this.props.userID}/workinfo`), {
+                work: this.state.workdata["work"],
+
+            });
+
+            set(ref(db, `${this.props.userID}/skillsinfo`), {
+                skills: this.state.skillsdata["skills"],
+            });
+
+
+            set(ref(db, `${this.props.userID}/projectsinfo`), {
+                projects: this.state.projectsdata["projects"],
+            });
+
+            set(ref(db, `${this.props.userID}/galleryinfo`), {
+                gallery: this.state.gallerydata["gallery"],
+            });
+
+
+            set(ref(db, `${this.props.userID}/certificatesinfo`), {
+                certificates: this.state.certificatesdata["certificates"],
+            });
+            alert("Submitted details successfully");
+        }
+        else {
             alert('Please login first.');
             this.props.navigate('/')
             return;
         }
-        const db = getDatabase();
-        console.log(this.props.userID);
-        set(ref(db, `${this.props.userID}/information`), {
-            name: this.state.informationdata["name"],
-            domain: this.state.informationdata["domain"],
-            image: this.state.informationdata["image"],
-            email: this.state.informationdata["email"],
-            whoami: this.state.informationdata["whoami"],
-            description: this.state.informationdata["description"],
-            location: this.state.informationdata["location"],
-        });
-        console.log(this.state.socialprofilesdata.profiles.length);
-        console.log(this.state.socialprofilesdata.profiles[0]["media"]);
-        set(ref(db, `${this.props.userID}/socialmediaprofilesinfo`), {
-            profiles: this.state.socialprofilesdata["profiles"],
-        });
-        set(ref(db, `${this.props.userID}/educationinfo`), {
-            education: this.state.educationdata["education"],
-        });
-
-
-        set(ref(db, `${this.props.userID}/workinfo`), {
-            work: this.state.workdata["work"],
-
-        });
-
-        set(ref(db, `${this.props.userID}/skillsinfo`), {
-            skills: this.state.skillsdata["skills"],
-        });
-
-
-        set(ref(db, `${this.props.userID}/projectsinfo`), {
-            projects: this.state.projectsdata["projects"],
-        });
-
-        set(ref(db, `${this.props.userID}/galleryinfo`), {
-            gallery: this.state.gallerydata["gallery"],
-        });
-
-
-        set(ref(db, `${this.props.userID}/certificatesinfo`), {
-            certificates: this.state.certificatesdata["certificates"],
-        });
-        alert("Submitted details successfully");
     }
 
     render() {
         return (
+            // (this.state.read == true) ?
             <>
                 <FormNav />
                 <Fragment>
@@ -102,6 +205,14 @@ class Form extends React.Component {
                         }}>
                             <h1>Form</h1>
                             <p >Fill the required details to have a preview of the website</p>
+                            <Button
+                                variant="contained"
+                                sx={{ mt: 3, mb: 2, display: "flex", alignSelf: "center" }}
+                                // onChange={(data) => setInformationData(data)}
+                                onClick={() => this.initializeData()}
+                            >
+                                Fill Data
+                            </Button>
                         </header>
                     </div>
 
@@ -133,7 +244,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={informationschema}
                                         uischema={informationuischema}
-                                        data={this.state.informationdata}
+                                        data={infoFlag == true ? infoData : this.state.informationdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -153,7 +264,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={socialprofilesschema}
                                         uischema={socialprofilesuischema}
-                                        data={this.state.socialprofilesdata}
+                                        data={socialFlag == true ? socialData : this.state.socialprofilesdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -173,7 +284,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={educationschema}
                                         uischema={educationuischema}
-                                        data={this.state.educationdata}
+                                        data={educationFlag == true ? educationData : this.state.educationdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -193,7 +304,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={workschema}
                                         uischema={workuischema}
-                                        data={this.state.workdata}
+                                        data={workFlag == true ? workData : this.state.workdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -213,7 +324,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={skillsschema}
                                         uischema={skillsuischema}
-                                        data={this.state.skillsdata}
+                                        data={skillsFlag == true ? skillsData : this.state.skillsdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -233,7 +344,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={certificatesschema}
                                         uischema={certificatesuischema}
-                                        data={this.state.certificatesdata}
+                                        data={certificatesFlag == true ? certificatesData : this.state.certificatesdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -253,7 +364,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={projectsschema}
                                         uischema={projectsuischema}
-                                        data={this.state.projectsdata}
+                                        data={projectsFlag == true ? projectsData : this.state.projectsdata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -273,7 +384,7 @@ class Form extends React.Component {
                                     <JsonForms
                                         schema={galleryschema}
                                         uischema={galleryuischema}
-                                        data={this.state.gallerydata}
+                                        data={galleryFlag == true ? galleryData : this.state.gallerydata}
                                         renderers={materialRenderers}
                                         cells={materialCells}
                                         onChange={({ _errors, data }) => {
@@ -298,6 +409,14 @@ class Form extends React.Component {
                     <DownloadFooter />
                 </Fragment>
             </>
+            // :
+            // <>
+            //     <Typography>
+            //         Loading
+            //     </Typography>
+            // </>
+
+
         );
     }
 }
