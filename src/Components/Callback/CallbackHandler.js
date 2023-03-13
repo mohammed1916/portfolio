8ed01f1c9270f8335f1c4f83f4b0f0b665844f80
Mohammed <arrahmaan9901@gmail.com>
2023-03-13 19:52:20 +0530

    Access code Obtained 🎉

--- src/Components/Callback/CallbackHandler.js
+++ src/Components/Callback/CallbackHandler.js
@@ -1,32 +1,39 @@
 import * as React from 'react'
 export default function CallbackHandler() {
     const codeParam = new URL(window.location.href).searchParams;
-    let [code, setCode] = React.useState(codeParam.get('code'));
+    const auth = codeParam.get('code');
+    let [error, setError] = React.useState("");
+    const errorCode = "🛠️ Error has occured, please retry after sometime.";
 
-    localStorage.setItem('linkedinAuthCode', code);
-
-    const url1 = `http://localhost:8080/${code}`;
-    if (code !== undefined || code !== "ERROR")
+    localStorage.setItem('linkedinAuthCode', auth);
+    let code = "";
+    const url1 = `http://localhost:8080/${auth}`;
+    if (code !== undefined || code !== "") {
         try {
             fetch(url1, { mode: 'cors' })
                 .then(res => {
                     res.text().then(res => {
-                        console.log("🎉 data ", res);
+                        console.log("🎉 res ", res);
                         if (res === "ERROR") {
-                            setCode("🛠️ Error has occured, please retry after sometime.");
-                            console.log("🛠️ code: ", code)
+                            setError(errorCode);
+                            console.log("🛠️ code: ", code, "---", error)
                         }
-                        else
+                        else {
                             localStorage.setItem('linkedinAccessCode', res)
+                            window.close();
+                        }
                     })
                 })
         } catch (err) {
-            setCode("🛠️ Error has occured, please retry after sometime." + err);
+            setError(errorCode);
+            console.log(error + err)
         }
-
+    } else {
+        console.log("skip")
+    }
     return (
         <>
-            {code}
+            {error !== errorCode ? "Redirecting" : errorCode}
         </>
     );
 }
