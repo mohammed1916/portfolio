5a2a703de5027541c9e471133b4bd41de8d94fd0
Mohammed <arrahmaan9901@gmail.com>
2023-03-27 11:03:26 +0530

    Typo Fix, YOP to Titile

--- src/Components/Form/work.js
+++ src/Components/Form/work.js
@@ -11,7 +11,7 @@ export const workschema = {
                 "type": "string",
             },
             "title": {
-                "title": "Year of Passing",
+                "title": "Title",
                 "type": "string",
             },
             "thumbnail": {
