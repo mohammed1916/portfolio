4bda30e159fb94494d8728ceea27f4f9c7151628
Mohammed <arrahmaan9901@gmail.com>
2023-05-04 18:21:00 +0530

    removed react-image-gallery and add noImg.. -works

--- src/Components/Projects/ProjectPage.js
+++ src/Components/Projects/ProjectPage.js
@@ -1,10 +1,13 @@
+
+import * as React from 'react';
 import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box'
-import ImageList from '@mui/material/ImageList';
-import ImageListItem from '@mui/material/ImageListItem';
+import Card from '@mui/material/Card';
+import CardMedia from '@mui/material/CardMedia';
+import { CardActionArea } from '@mui/material';
+import noImageAvailable from '../../img/icons/noImageAvailable.jpeg'
 
-import ImageGallery from 'react-image-gallery';
 
 import { data } from '../../data'
 
@@ -57,20 +60,40 @@ export default function ProjectPage() {
                         <Typography gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
                             Sample Images:
                         </Typography>
-                        {/* <ImageList variant="masonry" cols={3} gap={8} display={'flex'} aligncontents={'center'}>
-                            {data.projects[params.i].gallery.map((item) => (
-                                <ImageListItem key={item}>
-                                    <img
-                                        src={`${item}?w=248&fit=crop&auto=format`}
-                                        srcSet={`${item}?w=248&fit=crop&auto=format&dpr=2 2x`}
-                                        loading="lazy"
-                                    />
-                                </ImageListItem>
+                        <Box display={'flex'} flexWrap={'wrap'} justifyContent='space-evenly' bgcolor={'#eee'} borderRadius={'20px'} padding={'10px'}>
+                            {data.projects[params.i].gallery.map((item, index) => (
+                                <Box display={'flex'} flexWrap={'wrap'} justifyContent='center' >
+                                    <Card key={index} sx={{ maxWidth: 250, padding: '10px', margin: '20px', ":hover": "boxShadow: 0 15px 70px -12px rgba(0,0,0,0.3) " }} >
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="480"
+                                                image={item[0] ? item[0] : noImageAvailable}
+                                            />
+                                        </CardActionArea>
+                                    </Card>
+                                    <Card key={index} sx={{ maxWidth: 250, padding: '10px', margin: '20px', ":hover": "boxShadow: 0 15px 70px -12px rgba(0,0,0,0.3) " }} >
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="480"
+                                                image={item[1] ? item[1] : noImageAvailable}
+                                            />
+                                        </CardActionArea>
+                                    </Card>
+                                    <Card key={index} sx={{ maxWidth: 250, padding: '10px', margin: '20px', ":hover": "boxShadow: 0 15px 70px -12px rgba(0,0,0,0.3) " }} >
+                                        <CardActionArea>
+                                            <CardMedia
+                                                component="img"
+                                                height="480"
+                                                image={item[2] ? item[2] : noImageAvailable}
+                                            />
+                                        </CardActionArea>
+                                    </Card>
+                                </Box>
                             ))}
-                        </ImageList> */}
-                        <Box bgcolor={'eee'} borderRadius={'20px'} padding={'10px'}>
-                            <ImageGallery items={data.projects[params.i].gallery} />
                         </Box>
+
                     </Box>
                 </Container>
             </Box>
