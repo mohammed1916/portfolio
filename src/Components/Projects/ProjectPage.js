4ecc1ed66edeb37b81a2b53983aa39553b805d12
Mohammed <arrahmaan9901@gmail.com>
2024-05-16 10:30:12 +0530

    Refactor ProjectPage.js font and ui padding

--- src/Components/Projects/ProjectPage.js
+++ src/Components/Projects/ProjectPage.js
@@ -3,6 +3,7 @@ import * as React from 'react';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
 import Card from '@mui/material/Card';
 import CardMedia from '@mui/material/CardMedia';
 import { CardActionArea } from '@mui/material';
@@ -26,39 +27,39 @@ export default function ProjectPage()
                 bgcolor={'white'}
                 color={'white'} >
                 <Container sx={{ width: '100%' }} fontFamily={'ZCOOL XiaoWei'} >
-                    <Typography textAlign="center" fontFamily={'Be Vietnam Pro'} fontSize={'40px'} color={'black'} pt={{ xs: 1, sm: 2 }}>PROJECT</Typography>
-                    <Box display={'flex'} flexDirection={'column'} bgcolor={'#333'} borderRadius={'20px'} padding={'10px'} boxShadow={10}>
+                    <Typography textAlign="center" fontFamily={'Be Vietnam Pro'} fontSize={'40px'} color={'black'} p={{ xs: 1, sm: 2 }}>PROJECT</Typography>
+                    <Box display={'flex'} flexDirection={'column'} bgcolor={'#333'} borderRadius={'20px'} padding={'20px'} boxShadow={10}>
                         <Typography fontFamily={'ZCOOL XiaoWei'} gutterBottom variant="h3" component="div" textAlign={'center'} paddingBottom={'8px'}>
                             {data.projects[params.i].title}
                         </Typography>
 
                         <Box display={'flex'} flexDirection={'row'} >
-                            <Typography gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                            <Typography fontFamily={'Gilroy Bold'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
                                 {`Application Type:`}
                             </Typography>
-                            <Typography gutterBottom variant="h6" component="div"  >
+                            <Typography fontFamily={'Gilroy Light'} gutterBottom variant="h6" component="div"  >
                                 {`${data.projects[params.i].type}`}
                             </Typography>
                         </Box>
                         <Box display={'flex'} flexDirection={'row'} >
-                            <Typography gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                            <Typography fontFamily={'Gilroy Bold'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
                                 {`Link: `}
                             </Typography>
-                            <a href={data.projects[params.i].link}>
-                                <Typography color={'white'} gutterBottom variant="h6" component="div"  >
+                            <Link href={data.projects[params.i].link} underline="hover" aria-label="Explore Project">
+                                <Typography fontFamily={'Gilroy Light'} color={'white'} gutterBottom variant="h6" component="div">
                                     {`${data.projects[params.i].link}`}
                                 </Typography>
-                            </a>
+                            </Link>
                         </Box>
                         <Box display={'flex'} flexDirection={'row'} >
-                            <Typography gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                            <Typography fontFamily={'Gilroy Bold'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
                                 {`Description: `}
                             </Typography>
-                            <Typography gutterBottom variant="h6" component="div"  >
+                            <Typography fontFamily={'Gilroy Light'} gutterBottom variant="h6" component="div"  >
                                 {`${data.projects[params.i].description}`}
                             </Typography>
                         </Box>
-                        <Typography gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'}>
+                        <Typography fontFamily={'Gilroy Bold'} gutterBottom variant="h6" component="div" style={{ fontWeight: 600 }} paddingRight={'5px'} paddingTop={'30px'}>
                             Sample Images:
                         </Typography>
                         <Box display={'flex'} flexWrap={'wrap'} justifyContent='space-evenly' bgcolor={'#eee'} borderRadius={'20px'} padding={'10px'}>
