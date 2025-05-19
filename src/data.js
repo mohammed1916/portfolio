3568cca93c821a5ebe995f8ba4c2872b7265ffc4
Mohammed Abdullah <arrahmaan9901@gmail.com>
2025-05-19 17:35:30 +0530

    formatted linebreaks, and use br for next line in json with html rendering

--- src/data.js
+++ src/data.js
@@ -61,7 +61,7 @@ export const data = {
             "title": "Intern",
             "thumbnail": require('./Components/Certifications/Images/Internship~The Sparks Foundation~Hybrid Mobile App.png'),
             "dates": "Sept - Oct 2021",
-            "description": "Worked on creating a Hybrid Mobile App using Flutter.\nCreated several models in database.\nPerformed CRUD operations"
+            "description": "Worked on creating a Hybrid Mobile App using Flutter.<br >Created several models in database.<br >Performed CRUD operations"
         }
     ],
     "skills": [
@@ -155,38 +155,7 @@ export const data = {
         },
     ],
     "certifications": [
-        {
-            "title": "Project on A Novel Method for Handwritten Digit Recognition System",
-            "date": "February 20, 2023",
-            "Institution": "IBM Developer Skills Network",
-            "thumbnail": require('./Components/Certifications/Images/Course~IBM~HandwrittenDigitRecognition.png'),
-            "description": `The project can be found at: https://github.com/mohammed1916/IBM-Project-3542-1658575041/tree/main/Final%20Deliverables
-            
-            DESCRIPTION: 
-            Language: Python
-            Framework: Flask
-            Libraries: tensorflow, numpy, matplotlib, sklearn
-            
-            Worked in a team with Agile SDLC to design, create, train, and test an ML model.
-            
-            Note: The project was submitted as part of the Nalaiya Thiran programme partnered with IBM. Credentials: https://courses.ictacademy.skillsnetwork.site/certificates/98dcd21d a1ee4de49b07df44df0da0d0 
-            
-            FEATURES: Deployed ML model using Flask App which could be tunnelled to an external URL using Ngrok to execute the program in Google Colab.
-            
-            MODEL:
-            The ML model was created by importing MNIST data set and reshaping the images, which were divided into training and testing datasets.
-            This was then passed through a CNN with 2D convolution layer with ReLU as its activation function to combat vanishing gradients, and then downsampled using MaxPooling layer followed by a flatten and two dense layers and finally compiled using SGD optimizer and categorical cross entropy loss function.
-            This was exported in h5 format and saved in the static folder of the flask application.
-            
-            FLASK APPLICATION:
-            The model was loaded into the Flask application by importing the exported model. This was then used to determine the digit from the uploaded image, as recognized by the model and to calculate the accuracy.
-            The root '/' during GET renders the index.html file present in the templates folder. When a POST request is made after determining the digit, the power of jinja statements is utilised in dynamic rerendering of the html page.
-            During this process the request library is used to obtain the uploaded image.
-            
-            
-            TESTING: load testing was performed using locust for POST where multiple locust Tasks were created in which the HttpSession client uploaded test images to validate for HTTP 400 OK response.
-            `
-        },
+        
         {
             "title": "Kotlin For Java Developers",
             "date": "July 22, 2021",
@@ -199,14 +168,14 @@ export const data = {
             "date": "October 20, 2024 - expires on March 16, 2026",
             "Institution": "Nvidia",
             "thumbnail": require('./Components/Certifications/Images/Course~Nvidia~C++Cuda.png'),
-            "description": "Obtained Certificate of Competency for CUDA C/C++ \n Credential: https://learn.nvidia.com/certificates?id=p837-4HkR1mmR-5G-QROOA \nI gained a solid foundation in GPU programming using CUDA. The course began with an introduction and setup, where I registered on the NVIDIA platform to access course materials. I then learned the essential CUDA syntax and concepts required to write, compile, and run GPU-enabled C/C++ applications. This included understanding thread hierarchies, and how to allocate and manage GPU memory. \n Further, I explored memory management techniques using CUDA-managed (unified) memory. I learned to profile CUDA applications using the command-line profiler and gained insights into optimizing memory behavior based on profiling results. The course also covered asynchronous execution with CUDA streams and how to use NVIDIA Nsight Systems for visual profiling, which helped me identify opportunities for overlapping data transfers and computation. \n Finally, I reviewed all key learnings and completed an assessment to earn a certificate. This course significantly strengthened my ability to develop and optimize GPU-accelerated applications using CUDA C/C++."
+            "description": "Obtained Certificate of Competency for CUDA C/C++ <br > Credential: https://learn.nvidia.com/certificates?id=p837-4HkR1mmR-5G-QROOA <br >I gained a solid foundation in GPU programming using CUDA. The course began with an introduction and setup, where I registered on the NVIDIA platform to access course materials. I then learned the essential CUDA syntax and concepts required to write, compile, and run GPU-enabled C/C++ applications. This included understanding thread hierarchies, and how to allocate and manage GPU memory. <br > Further, I explored memory management techniques using CUDA-managed (unified) memory. I learned to profile CUDA applications using the command-line profiler and gained insights into optimizing memory behavior based on profiling results. The course also covered asynchronous execution with CUDA streams and how to use NVIDIA Nsight Systems for visual profiling, which helped me identify opportunities for overlapping data transfers and computation. <br > Finally, I reviewed all key learnings and completed an assessment to earn a certificate. This course significantly strengthened my ability to develop and optimize GPU-accelerated applications using CUDA C/C++."
         },
         {
             "title": "Fundamentals of Accelerated Computing with CUDA Python",
             "date": "October 23, 2024",
             "Institution": "Nvidia",
             "thumbnail": require('./Components/Certifications/Images/Course~Nvidia~PythonCuda.png'),
-            "description": "Obtained Certificate of Competency for CUDA C/C++ \n Credential: https://learn.nvidia.com/certificates?id=fV0VTTEgRMqkL7nJ4oDgSQ \nThe course began with a brief introduction and setup via the NVIDIA Learning platform. I was then introduced to the Numba compiler and learned how to use Numba decorators to GPU-accelerate numerical Python functions. Additionally, I explored best practices for optimizing memory transfers between the host (CPU) and device (GPU). \n In the next phase, I learned how to write and launch custom CUDA kernels in Python. This included understanding CUDA’s parallel thread hierarchy and utilizing atomic operations to prevent race conditions during parallel execution. \n The course also covered the use of multidimensional grids and shared memory in CUDA Python. I applied these concepts to work efficiently on 2D matrices, leveraging shared memory to enhance performance through memory coalescing. \n Finally, I reviewed all the core concepts covered in the course, completed an assessment, and earned a certificate of completion. This workshop significantly enhanced my ability to build and optimize GPU-accelerated Python applications using Numba and CUDA."
+            "description": "Obtained Certificate of Competency for CUDA C/C++ <br > Credential: https://learn.nvidia.com/certificates?id=fV0VTTEgRMqkL7nJ4oDgSQ <br >The course began with a brief introduction and setup via the NVIDIA Learning platform. I was then introduced to the Numba compiler and learned how to use Numba decorators to GPU-accelerate numerical Python functions. Additionally, I explored best practices for optimizing memory transfers between the host (CPU) and device (GPU). <br > In the next phase, I learned how to write and launch custom CUDA kernels in Python. This included understanding CUDA’s parallel thread hierarchy and utilizing atomic operations to prevent race conditions during parallel execution. <br > The course also covered the use of multidimensional grids and shared memory in CUDA Python. I applied these concepts to work efficiently on 2D matrices, leveraging shared memory to enhance performance through memory coalescing. <br > Finally, I reviewed all the core concepts covered in the course, completed an assessment, and earned a certificate of completion. This workshop significantly enhanced my ability to build and optimize GPU-accelerated Python applications using Numba and CUDA."
         },
         {
             "title": "10-Day GPU Accelerated Computing and Codeathon",
@@ -234,21 +203,53 @@ export const data = {
             "date": "July - Oct, 2021",
             "Institution": "NPTEL",
             "thumbnail": require('./Components/Certifications/Images/Course~NPTEL~Deep Learning.jpg'),
-            "description": "Here is comprehensive TIMELINE of course:\nWeek 1: History of Deep Learning, Deep Learning Success Stories, McCulloch Pitts Neuron \nWeek 2: Multilayer Perceptrons (MLPs), Representation Power of MLPs, Sigmoid Neurons, Gradient Descent\nWeek 3: Feed Forward Neural Networks, Back propagation\nWeek 4: Gradient Descent (GD), Momentum Based GD, Nesterov Accelerated GD, Stochastic GD\nWeek 5: Principal Component Analysis and its interpretations, Singular Value Decomposition\nWeek 6: Auto encoders and relation to PCA Regularization in auto encoders, Denoising auto encoders, Sparse auto encoders\nWeek 7: Regularization: Bias Variance Tradeoff, L2 regularization Early stopping, Dataset augmentation\nWeek 8: Greedy Layerwise Pre-training, Better activation functions, Better weight initialization methods, Batch Normalization\nWeek 9: Learning Vectorial Representations Of Words\nWeek 10: Convolutional Neural Networks, LeNet, AlexNet, ZF-Net, VGGNet, GoogLeNet, ResNet\nWeek 11: Recurrent Neural Networks, Back propagation through time (BPTT), Vanishing and Exploding Gradients, Truncated BPTT, GRU, LSTMs\nWeek 12: Encoder Decoder Models, Attention Mechanism, Attention over images"
+            "description": "Credentials: https://archive.nptel.ac.in/noc/Ecertificate/?q=NPTEL21CS76S2335215203121592 <br > I recently completed a comprehensive deep learning course that offered both theoretical foundations and practical insights into modern neural network architectures and training techniques. <br > Week 1 covered the history of deep learning, its early breakthroughs, and the foundational McCulloch-Pitts neuron model. <br > In Week 2, I learned about Multilayer Perceptrons (MLPs), their expressive power, and the mechanics of sigmoid neurons and gradient descent. <br > Week 3 focused on feedforward neural networks and introduced the backpropagation algorithm for training. <br > Week 4 deepened this understanding by exploring various optimization techniques like momentum-based gradient descent, Nesterov accelerated gradient, and stochastic gradient descent. <br > In Week 5, I studied Principal Component Analysis (PCA) and Singular Value Decomposition (SVD) for dimensionality reduction and data interpretation. <br > Week 6 introduced autoencoders and their relation to PCA, including concepts like regularized, denoising, and sparse autoencoders. <br > Week 7 emphasized regularization techniques such as L2 regularization, early stopping, and data augmentation, focusing on the bias-variance tradeoff. <br > Week 8 explored advanced training strategies like greedy layerwise pre-training, improved activation functions, weight initialization, and batch normalization. <br > In Week 9, I delved into learning vectorial representations of words, a fundamental concept in NLP. <br > Week 10 covered Convolutional Neural Networks (CNNs) and their evolution through architectures like LeNet, AlexNet, ZF-Net, VGGNet, GoogLeNet, and ResNet. <br > Week 11 focused on Recurrent Neural Networks (RNNs), including BPTT, challenges like vanishing/exploding gradients, and architectures such as GRUs and LSTMs. <br > Finally, Week 12 concluded with Encoder-Decoder models and the attention mechanism, including its application to both sequences and images."
         },
         {
             "title": "Building Transformer-Based Natural Language Processing Applications",
             "date": "March 11, 2022",
             "Institution": "NVIDIA",
             "thumbnail": require('./Components/Certifications/Images/Course~NVIDIA~Building Transformer-Based Natural Language Processing Applications.jpg'),
-            "description": "I explored how the Transformer architecture works in detail and Learned how to apply self-supervised Transformer-based models to concrete NLP tasks using NVIDIA NeMo and finally deployed an NLP project for live inference on NVIDIA Triton"
+            "description": "Credentials: https://learn.nvidia.com/certificates?id=0f2d9871de1b49b18b4d9c8fbd018937 <br > I explored how the Transformer architecture works in detail and Learned how to apply self-supervised Transformer-based models to concrete NLP tasks using NVIDIA NeMo and finally deployed an NLP project for live inference on NVIDIA Triton"
         },
         {
             "title": "Fundamentals of Deep Learning",
             "date": "March 10, 2022",
             "Institution": "NVIDIA",
             "thumbnail": require('./Components/Certifications/Images/Course~NVIDIA~Fundamentals of Deep Learning.jpg'),
-            "description": "Learned to apply data augmentation to enhance a dataset and improve model generalization and also studied Pre-trained Models and Recurrent Networks and finally executed a project based on Object Classification"
+            "description": "Credential: https://learn.nvidia.com/certificates?id=6d11c293c3fc445f8f853ef77e000fd5 <br > Learned to apply data augmentation to enhance a dataset and improve model generalization and also studied Pre-trained Models and Recurrent Networks and finally executed a project based on Object Classification"
+        },
+        {
+            "title": "Project on A Novel Method for Handwritten Digit Recognition System",
+            "date": "February 20, 2023",
+            "Institution": "IBM Developer Skills Network",
+            "thumbnail": require('./Components/Certifications/Images/Course~IBM~HandwrittenDigitRecognition.png'),
+            "description": `The project can be found at: https://github.com/mohammed1916/IBM-Project-3542-1658575041/tree/main/Final%20Deliverables
+            
+            <br >DESCRIPTION: 
+            <br >Language: Python
+            <br >Framework: Flask
+            <br >Libraries: tensorflow, numpy, matplotlib, sklearn
+            
+            <br >Worked in a team with Agile SDLC to design, create, train, and test an ML model.
+            
+            <br >Note: The project was submitted as part of the Nalaiya Thiran programme partnered with IBM. Credentials: https://courses.ictacademy.skillsnetwork.site/certificates/98dcd21d a1ee4de49b07df44df0da0d0 
+            
+            <br ><br >FEATURES: Deployed ML model using Flask App which could be tunnelled to an external URL using Ngrok to execute the program in Google Colab.
+            
+            <br ><br >MODEL:
+            <br >The ML model was created by importing MNIST data set and reshaping the images, which were divided into training and testing datasets.
+            This was then passed through a CNN with 2D convolution layer with ReLU as its activation function to combat vanishing gradients, and then downsampled using MaxPooling layer followed by a flatten and two dense layers and finally compiled using SGD optimizer and categorical cross entropy loss function.
+            This was exported in h5 format and saved in the static folder of the flask application.
+            
+            <br ><br >FLASK APPLICATION:
+            <br >The model was loaded into the Flask application by importing the exported model. This was then used to determine the digit from the uploaded image, as recognized by the model and to calculate the accuracy.
+            The root '/' during GET renders the index.html file present in the templates folder. When a POST request is made after determining the digit, the power of jinja statements is utilised in dynamic rerendering of the html page.
+            During this process the request library is used to obtain the uploaded image.
+            
+            
+            TESTING: load testing was performed using locust for POST where multiple locust Tasks were created in which the HttpSession client uploaded test images to validate for HTTP 400 OK response.
+            `
         },
         {
             "title": "Java Course with Practical Examples for Absolute Beginners",
