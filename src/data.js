8c9477ea0b02b297d33c361fab14835d566275f4
Mohammed Abdullah <arrahmaan9901@gmail.com>
2025-08-20 22:55:44 +0530

    data: dynamic portfolio

--- src/data.js
+++ src/data.js
@@ -180,12 +180,12 @@ export const data = {
         },
         {
             "title": "Dynamic Portfolio generator",
-            "type": "Single Page Web App",
+            "type": "Frontend, MiddleWare, Backend, Socket Programming",
             "thumbnail": project2_1,
             "link": " https://mohammedabdullahportfolio.web.app/",
             "description": `Employed REST API's to perform 3-legged authentication for linkedIn.
             Scraped data from LinkedIn and Github using Puppeteer.
-            Used socket programming to send the scrapped data between middleware server and client. Project can be found at https://github.com/mohammed1916/portfolio/tree/FirebaseWebsite`,
+            Used socket programming to send the scrapped data between middleware server and client. Project can be found at https://github.com/mohammed1916/Dynamic-Portfolio-Generator`,
             "gallery": [
                 {
                     original: project2_1,
